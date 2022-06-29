6ba6146d538bf266807ee65e22701c3d18c0c13e
julio cesar arrieta santiago <jarrieta@finanzas.cdmx.gob.mx>
2022-06-29 16:45:42 -0500

    icon searach, card center

--- src/componets/BuscadorForm.jsx
+++ src/componets/BuscadorForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import  useMercado  from "../hooks/useMercado"
+import logo_mercado from '../assets/Logo_ML.png' 
 
 const BuscadorForm = () => {
 
@@ -22,24 +23,42 @@ const BuscadorForm = () => {
     return (
         <div >
             {alerta && <p>{alerta}</p>}
-            <form
-            onSubmit={handleSubmit}
-            >
-                <div className="campo">
-                  
-                    <input
-                    type="text"
-                    id="ciudad"
-                    name="ciudad"
-                    onChange={datosBusqueda}
-                    value={ciudad}
-                    />
+            <div className="row">
+            <div className="col-lg-1"></div>
+                <div className="col-lg-10">    
+                    <form onSubmit={handleSubmit} >
+                            <div className="row">
+                            <div className="col-lg-1">
+                            <img src={logo_mercado} className="nav-img" width="60" height="40"  ></img>
+                            </div>
+                            <div className="col-lg-11">
+                                <div className="input-group">
+                                    <input className="form-control"
+                                    type="text"
+                                    id="ciudad"
+                                    name="ciudad"
+                                    placeholder="Nunca dejes de buscar"
+                                    onChange={datosBusqueda}
+                                    value={ciudad}
+                                    />
+                                    <div className="input-group-append">
+                                        <span className="input-group-text">
+                                        <button
+                                        type="submit" 
+                                        value="Consulta Clima">
+                                            <i className="bi bi-search"  aria-label="GitHub"></i>
+                                        </button>
+                                        </span>
+                                    </div>
+                                </div>
+                            
+                            </div>
+                            </div>
+                    </form>
+
                 </div>
-                <input
-                type="submit" 
-                value="Consulta Clima"
-                />
-            </form>
+                <div className="col-lg-1"></div>
+            </div>
         </div>
     );
 }
--- src/componets/ResultadoMercado.jsx
+++ src/componets/ResultadoMercado.jsx
@@ -1,5 +1,6 @@
 import useMercado from "../hooks/useMercado"
 import { Link } from "react-router-dom";
+import logo_mercado from '../assets/Logo_ML.png' 
 const ResultadoMercado = () => {
     
     const { resultado } = useMercado()
@@ -7,10 +8,17 @@ const ResultadoMercado = () => {
 
     return (
         <>
-            <h2>Resultados </h2> 
+           
+            <div className="row">
+            <div className="col-lg-1">
+            </div>
+                <div className="col-lg-8">
+                Resultados>blablaca>rbbbbb>bgbgbgbg
+                </div>
+            </div>
 
         {results.map((rr) =>
-            <div  key={rr.id} className="slick-initialized slick-slider">
+            /*<div  key={rr.id} className="slick-initialized contenedor">
                 <div  key={rr.id} role="presentation" className="slick-list">      
                     <div key={rr.id} className="slick-track">
                         <div key={rr.id} data-index="0" className="slick-slide slick-active" tabIndex="-1" > 
@@ -23,7 +31,30 @@ const ResultadoMercado = () => {
                         </div>
                     </div>
                 </div>
-            </div>    
+            </div> */   
+
+            /*<div key={rr.id} className="card-init" >
+                <img src={rr.thumbnail} className="card-img" ></img>
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>*/
+            <div className="content">
+            <div className="tarjeta">
+            <div  key={rr.id} className="card">
+            <img src={rr.thumbnail} className="card-img-top" ></img>
+            <div  key={rr.id} className="card-body">
+              <h5  className="card-title">${rr.price}</h5> <img src={logo_mercado} className="nav-img" width="60" height="40"  ></img>
+              <p className="card-text">
+              {rr.title}
+              </p>
+              <a href="#" className="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+          </div>
+          </div>
               )}
             
             
