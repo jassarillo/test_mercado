8dd3e5d08b5fb9e23675c52dbc1b01a491a8ba05
julio cesar arrieta santiago <jarrieta@finanzas.cdmx.gob.mx>
2022-06-29 18:42:16 -0500

    icono png a jpg

--- src/componets/ResultadoMercado.jsx
+++ src/componets/ResultadoMercado.jsx
@@ -1,6 +1,8 @@
 import useMercado from "../hooks/useMercado"
 import { Link } from "react-router-dom";
 import logo_mercado from '../assets/Logo_ML.png' 
+import logo_envio from '../assets/ic_shipping@2x.png.png' 
+import ic_shipping from '../assets/ic_shipping.jpg' 
 const ResultadoMercado = () => {
     
     const { resultado } = useMercado()
@@ -9,53 +11,39 @@ const ResultadoMercado = () => {
     return (
         <>
            
-            <div className="row">
+            <div className="row row_ruta_articulo">
             <div className="col-lg-1">
             </div>
                 <div className="col-lg-8">
                 Resultados>blablaca>rbbbbb>bgbgbgbg
                 </div>
             </div>
-
-        {results.map((rr) =>
-            /*<div  key={rr.id} className="slick-initialized contenedor">
-                <div  key={rr.id} role="presentation" className="slick-list">      
-                    <div key={rr.id} className="slick-track">
-                        <div key={rr.id} data-index="0" className="slick-slide slick-active" tabIndex="-1" > 
-                                    key={rr.title}
-                                    <img key={rr.id} width="160" height="160"
-                                            src={rr.thumbnail}
-                                            className="ui-search-result-image__element" alt="Aprendiz De Caballero 2
-                                            Una Yegua Llamada Dora * Edelvives">
-                                    </img>
+            <div className="content">
+            {results.map((rr) =>
+               
+                
+                <div className="tarjeta">
+                    <div  key={rr.id} className="card">
+                        <img src={rr.thumbnail} className="img_list" ></img>
+                        <div  key={rr.id} className="card-body">
+                            <span  className="card-title">
+                                ${rr.price}
+                            </span> 
+                            
+                            <div >
+                            <img src={ic_shipping}  id="icono_envio" ></img>eded
+                            </div>
+                                
+                            <p className="card-text">
+                            {rr.title}
+                            </p>
+                            <a href="#" className="btn btn-primary">Go somewhere</a>
                         </div>
                     </div>
                 </div>
-            </div> */   
-
-            /*<div key={rr.id} className="card-init" >
-                <img src={rr.thumbnail} className="card-img" ></img>
-                <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
-                </div>
-            </div>*/
-            <div className="content">
-            <div className="tarjeta">
-            <div  key={rr.id} className="card">
-            <img src={rr.thumbnail} className="card-img-top" ></img>
-            <div  key={rr.id} className="card-body">
-              <h5  className="card-title">${rr.price}</h5> <img src={logo_mercado} className="nav-img" width="60" height="40"  ></img>
-              <p className="card-text">
-              {rr.title}
-              </p>
-              <a href="#" className="btn btn-primary">Go somewhere</a>
+            
+                )}
             </div>
-          </div>
-          </div>
-          </div>
-              )}
             
             
         </>
